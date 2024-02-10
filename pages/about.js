c55a163dc61179aa106cbb6219464be9111a401c
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-10 16:52:21 +0330

    styling components by global or module css

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from "../styles/About.module.css";
 
 export default function About() {
   return (
     <>
-      <h1>about</h1>
+      <h1 className={styles.header}>about</h1>
       <Link href="/">Main page</Link>
     </>
   );
