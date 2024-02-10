a101d7b0f8aa4836f2606638ca51c8b2626500f0
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-10 16:57:23 +0330

    styled jsx

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,13 @@ export default function About() {
   return (
     <>
       <h1 className={styles.header}>about</h1>
+      <p>I&apos;m Amir Hossein Qorbani</p>
       <Link href="/">Main page</Link>
+      <style jsx>{`
+        p {
+          color: red;
+        }
+      `}</style>
     </>
   );
 }
