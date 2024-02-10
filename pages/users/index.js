99f29359bc3819cb93af1c09815d95a1625f9100
Amir Hossein Qorbani <a.qorbani.eng@gmail.com>
2024-02-10 16:14:46 +0330

    dynamic routing with Link component

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
+
+const users = [
+  { id: 1, name: 1 },
+  { id: 2, name: 2 },
+  { id: 3, name: 3 },
+  { id: 4, name: 4 },
+];
+
 export default function Users() {
   return (
     <>
       <h2>Users List</h2>
-      <a>user 1</a>
-      <a>user 2</a>
+      {users.map((item) => (
+        <div key={item.id}>
+          <Link href={`/users/${item.id}`}>user {item.name}</Link>
+        </div>
+      ))}
     </>
   );
 }
